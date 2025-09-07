2300ceeb544c9db9815b29b39ebd089e2bf36b92
JM CARABALI <jmcarabalir@gmail.com>
2025-09-06 19:32:56 -0500

    Agregar archivos de proyecto.

---
+++ COLEGIO_MATRICULA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COLEGIO_MATRICULA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
